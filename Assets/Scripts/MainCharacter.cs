2a396a444bc0b6cd6b59081400feccb9c61d15e1
ulanzetz <ulanzet@gmail.com>
2018-04-07 13:41:30 +0500

    Add death scene

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 class MainCharacter : MonoBehaviour
 {
     public float MovementSpeed = 10f;
     public float JumpSpeed = 10f;
 
-    //private Transform cachedTransform;
+    private Transform cachedTransform;
     private Rigidbody2D rb;
 
-    private void Start() => rb = GetComponent<Rigidbody2D>();
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cachedTransform = transform;
+    }
 
     private void Update()
     {
         var speedY = Input.GetKeyDown(KeyCode.Space) ? JumpSpeed : 0f;
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * MovementSpeed, speedY));
+        if (cachedTransform.position.y < 0)
+            SceneManager.LoadScene("DeathScene");
     }
 
 }
---
+++ Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+class PlayButton : MonoBehaviour
+{
+    private void Start() => GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene("Level1"));
+}
