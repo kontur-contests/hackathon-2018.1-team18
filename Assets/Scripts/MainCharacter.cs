3eecc256afcd8bed21b5fa75481260f751e548e7
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2018-04-07 23:27:00 +0600

    stagechangefix

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -63,7 +63,10 @@ class MainCharacter : MonoBehaviour
         if (cachedTransform.position.y < DeathY)
             SceneManager.LoadScene("DeathScene");
         if (Input.GetKeyDown(KeyCode.U))
-            Stage = 2;
+            Stage++;
+        else if (Input.GetKeyDown(KeyCode.J))
+            Stage--;
+
         else if (Input.GetKeyDown(KeyCode.Space))
         {
             if (canMove)
