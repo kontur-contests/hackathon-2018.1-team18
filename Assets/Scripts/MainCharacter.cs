dfd1d7caf9537fd95074ee542112bdebb559ff0a
ulanzetz <ulanzet@gmail.com>
2018-04-07 13:53:33 +0500

    Add jump stages

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -5,9 +5,11 @@ class MainCharacter : MonoBehaviour
 {
     public float MovementSpeed = 10f;
     public float JumpSpeed = 10f;
+    public int Stage = 1;
 
     private Transform cachedTransform;
     private Rigidbody2D rb;
+    private int jumpCount = 0;
 
     private void Start()
     {
@@ -17,10 +19,21 @@ class MainCharacter : MonoBehaviour
 
     private void Update()
     {
-        var speedY = Input.GetKeyDown(KeyCode.Space) ? JumpSpeed : 0f;
+        var speedY = 0f;
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCount++ < Stage)
+                speedY = JumpSpeed;
+        }
         rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * MovementSpeed, speedY));
         if (cachedTransform.position.y < 0)
             SceneManager.LoadScene("DeathScene");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (jumpCount != 0 && collision.gameObject.tag == "Ground")
+            jumpCount = 0;
     }
 
 }
