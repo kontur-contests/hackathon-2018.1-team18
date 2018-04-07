cc2ea6bacfeda7de588797a84b7b11aa0e393a8a
ulanzetz <ulanzet@gmail.com>
2018-04-07 20:07:42 +0500

    Dust and some refactoring

---
+++ Assets/Dust.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+class Dust : MonoBehaviour
+{
+    private const string LastFrame = "Dust_3";
+
+    private SpriteRenderer sp;
+
+    private void Start() => sp = GetComponent<SpriteRenderer>();
+
+    private void Update()
+    {
+        if (sp.sprite.name == LastFrame)
+            Destroy(gameObject);
+    }
+}
