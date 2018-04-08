9417983cde4d755851103e5a710e46c8d57ca7dd
ulanzetz <ulanzet@gmail.com>
2018-04-08 06:16:43 +0500

    +Attack

---
+++ Assets/Scripts/Blast.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+class Blast : MonoBehaviour
+{
+    public int Damage = 30;
+    public float Speed = 10;
+    public float Direction = 1f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(Direction * Speed, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        var damagable = collision.gameObject.GetComponent<IDamagable>();
+        if (damagable != null)
+            damagable.HP -= Damage;
+    }
+}
